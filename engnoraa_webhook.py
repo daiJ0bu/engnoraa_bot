82f8299fc61641669baca9bf127b87ee54a19818
Yahyobek <caisophiya@gmail.com>
2025-11-15 17:48:22 +0500

    added something

---
+++ engnoraa_webhook.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+from aiogram import Bot, Dispatcher, types
+import asyncio
+import os
+
+TOKEN = os.environ.get("AAEiUDItp344S3MmFGQIzInVO_nnPwhDSjs")  
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+app = Flask(__name__)
+
+@dp.message()
+async def start(message: types.Message):
+    if message.text == "/start":
+        await message.answer("🤖 Engnoraa bot ishlayapti!")
+
+@app.route(f"/{TOKEN}", methods=["POST"])
+def webhook():
+    json_str = request.get_data().decode("utf-8")
+    update = types.Update.de_json(json_str)
+    asyncio.run(dp.process_update(update))
+    return "OK"
